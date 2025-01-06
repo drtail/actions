6eb53e6627c4dbd3268860fb6118c09246182c95
Humphrey Ahn <ahnsv@bc.edu>
2025-01-06 11:40:33 -0500

    style: apply prettier

--- pr-label/jest.config.js
+++ pr-label/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.test.ts"],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts', '!tests/**'],
-  coverageDirectory: 'coverage',
-}
+  collectCoverageFrom: ["src/**/*.ts", "!tests/**"],
+  coverageDirectory: "coverage",
+};
--- pr-label/src/index.ts
+++ pr-label/src/index.ts
@@ -9,62 +9,66 @@ import * as github from "@actions/github";
 import { minimatch } from "minimatch";
 
 export const run = async (): Promise<void> => {
-    try {
-        const labels = core.getInput("labels").split("\n");
-        const duplicatePatterns = core.getInput("duplicate-patterns").split("\n");
-        const token = core.getInput("token");
+  try {
+    const labels = core.getInput("labels").split("\n");
+    const duplicatePatterns = core.getInput("duplicate-patterns").split("\n");
+    const token = core.getInput("token");
 
-        if (!token) {
-            throw new Error("GitHub token is required");
-        }
-
-        if (!labels.length) {
-            throw new Error("Labels are required");
-        }
-
-        if (!duplicatePatterns.length) {
-            throw new Error("Duplicate patterns are required");
-        }
-
-        const octokit = github.getOctokit(token);
-        const { owner, repo, number } = github.context.issue;
-
-        const { data: pr } = await octokit.rest.pulls.get({
-            owner,
-            repo,
-            pull_number: number,
-        });
-
-        if (pr.draft) {
-            core.info("This is a draft PR. No label added.");
-            return;
-        }
-
-        const duplicate = duplicatePatterns.find((pattern) =>
-            pr.labels.map(({ name }) => name).some((name) => minimatch(name, pattern)),
-        );
-
-        if (duplicate) {
-            core.info(`PR #${number} already has a label matching the pattern "${duplicate}". No new labels added.`);
-            return;
-        }
-
-        const { data: currentLabels } = await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: number,
-            labels,
-        });
-        const currentLabelNames = currentLabels.map((label) => label.name);
-
-        core.info(
-            `Labels "${labels.join(", ")}" added to PR #${number}. Current labels on PR: "${currentLabelNames.join(
-                ", ",
-            )}"`,
-        );
-    } catch (error) {
-        core.setFailed((error as Error).message);
+    if (!token) {
+      throw new Error("GitHub token is required");
     }
+
+    if (!labels.length) {
+      throw new Error("Labels are required");
+    }
+
+    if (!duplicatePatterns.length) {
+      throw new Error("Duplicate patterns are required");
+    }
+
+    const octokit = github.getOctokit(token);
+    const { owner, repo, number } = github.context.issue;
+
+    const { data: pr } = await octokit.rest.pulls.get({
+      owner,
+      repo,
+      pull_number: number,
+    });
+
+    if (pr.draft) {
+      core.info("This is a draft PR. No label added.");
+      return;
+    }
+
+    const duplicate = duplicatePatterns.find((pattern) =>
+      pr.labels
+        .map(({ name }) => name)
+        .some((name) => minimatch(name, pattern)),
+    );
+
+    if (duplicate) {
+      core.info(
+        `PR #${number} already has a label matching the pattern "${duplicate}". No new labels added.`,
+      );
+      return;
+    }
+
+    const { data: currentLabels } = await octokit.rest.issues.addLabels({
+      owner,
+      repo,
+      issue_number: number,
+      labels,
+    });
+    const currentLabelNames = currentLabels.map((label) => label.name);
+
+    core.info(
+      `Labels "${labels.join(", ")}" added to PR #${number}. Current labels on PR: "${currentLabelNames.join(
+        ", ",
+      )}"`,
+    );
+  } catch (error) {
+    core.setFailed((error as Error).message);
+  }
 };
 
 run();
